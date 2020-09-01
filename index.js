8837f58a52ec9dffdb0cb8b96a4f888a049397b3
Steven Velez <stevendvelez@yahoo.com>
2020-09-01 17:46:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const grid = document.querySelector(".grid");
+const score = document.getElementById("score");
+const startButton = document.getElementById("start-button");
+let squares = document.querySelectorAll(".grid div");
+
+const width = 10;
+
+const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+];
+
+const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+];
+
+const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+];
+
+const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+];
+
+const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+];
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+let currentPosition = 4;
+let current = theTetrominoes[0][0];
+
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add("tetromino");
+    });
+}
+
+draw();
